19774690f895839ec4795d0e57f7fb35c641a091
caltoll <gruntyhug@gmail.com>
2014-01-12 19:12:19 +0000

    updated to match changes in board.cpp

--- board.h
+++ board.h
@@ -11,11 +11,11 @@ public:
 	bool checkVictory(); //Check if the board is in a victory state & set getVictor											
 	void printBoard(); // Print the board with characters separated by | and _
 	bool makeMove(char place);
-	char getVictor(){return *victor;}
+	char getVictor(){return victor;}
 	void resetBoard();
 
 private: 
 	bool testArray(char array[3]); //As part of checkVictory: Test if an array of 3 characters are all equal
-	char * victor; //Char which shows which made the winning move, set via pointer through checkVictory
+	char victor; //Char which shows which made the winning move, set via pointer through checkVictory
 	char boardArray[3][3];
 };
