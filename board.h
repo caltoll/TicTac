aee1751058107483690c75d97f868b3089efdf3c
caltoll <gruntyhug@gmail.com>
2014-01-14 20:58:20 +0000

    minor layouting

--- board.h
+++ board.h
@@ -13,14 +13,14 @@ public:
 	bool checkVictory(); //Check if the board is in a victory state & set getVictor											
 	void printBoard(); // Print the board with characters separated by | and _
 	bool makeMove(char place, int move);
-        void resetBoard();
+    void resetBoard();
         
 	char getVictor(){return victor;}
-        std::string getStatus(){return status;}
+    std::string getStatus(){return status;}
 
 private: 
 	bool testArray(char array[3]); //As part of checkVictory: Test if an array of 3 characters are all equal
 	char victor; //Char which shows which made the winning move
-        std::string status;
+    std::string status;
 	char boardArray[3][3];
 };
